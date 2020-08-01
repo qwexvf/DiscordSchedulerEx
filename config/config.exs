ad521cab49f6d03a69fd4130602f4b5a612d1071
Marcelo <qw3xvf@gmail.com>
2020-08-01 13:15:02 +0900

    .

--- config/config.exs
+++ config/config.exs
@@ -47,7 +47,10 @@ config :exq,
   shutdown_timeout: 5000
 
 config :arc,
-  storage: Arc.Storage.Local
+storage: Arc.Storage.Local
+
+use Mix.Releases.Config,
+  default_release: :app
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
